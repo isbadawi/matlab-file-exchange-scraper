c6a1aae58d4ab2c8aca0a875ca2876556252366e
Ismail Badawi <isbadawi@gmail.com>
2013-09-24 01:47:40 -0400

    Simplify mkdir_p.

--- scrape.py
+++ scrape.py
@@ -26,9 +26,7 @@ def mkdir_p(path):
     try:
         os.makedirs(path)
     except OSError as e:
-        if e.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else:
+        if e.errno != errno.EEXIST or not os.path.isdir(path):
             raise
 
 
